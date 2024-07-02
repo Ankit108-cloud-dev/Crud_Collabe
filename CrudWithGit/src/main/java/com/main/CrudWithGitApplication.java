a4ace60caa82ec0cf250f30179e41fa7f33bd7f8
Ankit108-cloud-dev <ank108108108@gmail.com>
2024-07-02 15:23:23 +0530

    demo

---
+++ CrudWithGit/src/main/java/com/main/CrudWithGitApplication.java
@@ -0,0 +1,16 @@
+package com.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudWithGitApplication {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("This is crud Operation");
+		SpringApplication.run(CrudWithGitApplication.class, args);
+	}
+
+}
