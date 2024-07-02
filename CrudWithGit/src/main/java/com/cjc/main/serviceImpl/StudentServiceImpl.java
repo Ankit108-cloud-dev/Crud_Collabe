28fec75a5f51252b73c609e39229bae9b322487b
Ankit108-cloud-dev <ank108108108@gmail.com>
2024-07-02 16:08:20 +0530

    commit by Ankit.

--- CrudWithGit/src/main/java/com/cjc/main/serviceImpl/StudentServiceImpl.java
+++ CrudWithGit/src/main/java/com/cjc/main/serviceImpl/StudentServiceImpl.java
@@ -1,8 +1,22 @@
 package com.cjc.main.serviceImpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cjc.main.model.Student;
+import com.cjc.main.repository.StudentRepository;
 import com.cjc.main.service.StudentService;
 
+@Service
 public class StudentServiceImpl implements StudentService{
+	
+	@Autowired private StudentRepository stRepository;
+
+	@Override
+	public Iterable<Student> viewAllData() {
+		
+		return stRepository.findAll();
+	}
 	
 
 }
--- CrudWithGit/src/main/java/com/main/Controller/StudentController.java
+++ CrudWithGit/src/main/java/com/main/Controller/StudentController.java
@@ -1,23 +1,26 @@
 package com.main.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.cjc.main.model.Student;
 import com.cjc.main.service.StudentService;
 
-import ch.qos.logback.core.joran.spi.NoAutoStart;
-
+@Controller
 public class StudentController {
 	
 	@Autowired private StudentService st;
 	
-	@GetMapping("viewAllStudent")
-	public ResponseEntity<Student> viewData(@RequestBody Student student){
+	@GetMapping("/viewAllStudent")
+	public ResponseEntity<Iterable<Student>> viewData(){
 		
-		st.viewAllData(student);
+		Iterable<Student> student2=st.viewAllData();
+		
+		return new ResponseEntity<Iterable<Student>>(student2, HttpStatusCode.valueOf(200));
 		
 		
 	}
